f0462be561e1f1fc48785711f367a92b2f6b4d60
sodam.hwang <sodam.hwang@samsung.com>
2024-05-20 14:51:40 +0900

    TC refact

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,34 +1,41 @@
 ﻿#include "pch.h"
 #include "../Restaurant/BookingScheduler.cpp"
 
-TEST(Booking, 예약은_정시에만_가능하다_예외발생) {
-	tm notOntheHour = { 0 };
-	notOntheHour.tm_year = 2024 - 1900;
-	notOntheHour.tm_mon = 5 - 1;
-	notOntheHour.tm_mday = 20;
-	notOntheHour.tm_hour = 9;
-	notOntheHour.tm_min = 5;
-	mktime(&notOntheHour);
 
+class BookingItem : public testing::Test
+{
+protected:
+	void SetUp() override
+	{
+		NOT_ON_THE_HOUR = getTime(2024, 5, 20, 9, 5);
+		ON_THE_HOUR = getTime(2024, 5, 20, 9, 0);
+	}
+public:
+	tm getTime(int year,int mon, int day, int hour, int min)
+	{
+		tm result = { 0,min,hour,day,mon - 1, year - 1990,0,0,-1 };
+		mktime(&result);
+		return result;
+	}
+	tm NOT_ON_THE_HOUR;
+	tm ON_THE_HOUR;
+	Customer CUSTOMER{ "fake name", "010-1234-5678" };
+	const int UNDER_CAPACITY = 1;
+	const int CAPACITY_PER_HOUR= 3;
+	BookingScheduler bookingScheduler{ CAPACITY_PER_HOUR };
+
+};
+TEST_F(BookingItem, 예약은_정시에만_가능하다_예외발생) {
 	Customer customer{ "fake name", "010-1234-5678" };
-	Schedule* schedule = new Schedule{ notOntheHour, 1, customer };
-	BookingScheduler bookingScheduler{ 3 };
-
+	Schedule* schedule = new Schedule{ NOT_ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER };
+	
 	EXPECT_THROW(bookingScheduler.addSchedule(schedule), std::runtime_error);
 }
 
-TEST(Booking, 예약은_정시에만_가능하다_예약성공) {
-	tm ontheHour = { 0 };
-	ontheHour.tm_year = 2024 - 1900;
-	ontheHour.tm_mon = 5 - 1;
-	ontheHour.tm_mday = 20;
-	ontheHour.tm_hour = 9;
-	ontheHour.tm_min = 0;
-	mktime(&ontheHour);
+TEST_F(BookingItem, 예약은_정시에만_가능하다_예약성공) {
 
-	Customer customer{ "fake name", "010-1234-5678" };
-	Schedule* schedule = new Schedule{ ontheHour, 1, customer };
-	BookingScheduler bookingScheduler{ 3 };
+	Schedule* schedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER };
+	
 
 	bookingScheduler.addSchedule(schedule);
 	EXPECT_EQ(true,bookingScheduler.hasSchedule(schedule));
