f67de7e399d649837d7eca32a40b129dd1cfd2a2
sodam.hwang <sodam.hwang@samsung.com>
2024-05-20 14:40:33 +0900

    add tc

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,35 @@
-#include "pch.h"
+﻿#include "pch.h"
 #include "../Restaurant/BookingScheduler.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(Booking, 예약은_정시에만_가능하다_예외발생) {
+	tm notOntheHour = { 0 };
+	notOntheHour.tm_year = 2024 - 1900;
+	notOntheHour.tm_mon = 5 - 1;
+	notOntheHour.tm_mday = 20;
+	notOntheHour.tm_hour = 9;
+	notOntheHour.tm_min = 5;
+	mktime(&notOntheHour);
+
+	Customer customer{ "fake name", "010-1234-5678" };
+	Schedule* schedule = new Schedule{ notOntheHour, 1, customer };
+	BookingScheduler bookingScheduler{ 3 };
+
+	EXPECT_THROW(bookingScheduler.addSchedule(schedule), std::runtime_error);
+}
+
+TEST(Booking, 예약은_정시에만_가능하다_예약성공) {
+	tm ontheHour = { 0 };
+	ontheHour.tm_year = 2024 - 1900;
+	ontheHour.tm_mon = 5 - 1;
+	ontheHour.tm_mday = 20;
+	ontheHour.tm_hour = 9;
+	ontheHour.tm_min = 0;
+	mktime(&ontheHour);
+
+	Customer customer{ "fake name", "010-1234-5678" };
+	Schedule* schedule = new Schedule{ ontheHour, 1, customer };
+	BookingScheduler bookingScheduler{ 3 };
+
+	bookingScheduler.addSchedule(schedule);
+	EXPECT_EQ(true,bookingScheduler.hasSchedule(schedule));
 }
